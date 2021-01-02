162a535cffcf9d05f17a787ba7d12bb627f508d1
Anwesh43 <anweshthecool0@gmail.com>
2021-01-02 16:50:55 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedtextbarview/MainActivity.kt
+++ app/src/main/java/com/example/linkedtextbarview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedtextbarview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.textbarview.TextBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TextBarView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
