bef02605844beafe41f1daa3e85aea471fa04236
Anwesh43 <anweshthecool0@gmail.com>
2021-01-02 16:30:00 +0530

    adding canvas extension methods

--- textbarview/src/main/java/com/example/textbarview/TextBarView.kt
+++ textbarview/src/main/java/com/example/textbarview/TextBarView.kt
@@ -9,6 +9,7 @@ import android.graphics.RectF
 import android.app.Activity
 import android.content.Context
 
+val text : String = "2021"
 val parts : Int = 2
 val scGap : Float = 0.02f / parts
 val strokeFactor : Float = 90f
@@ -30,3 +31,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawTextBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val barH : Float = Math.min(w, h) / barHFactor
+    val fontSize : Float = Math.min(w, h) / fontFactor
+    paint.textSize = fontSize
+    val tw : Float = paint.measureText(text)
+    save()
+    translate(0f, h)
+    drawRect(RectF(0f, -barH * sf1, w, 0f), paint)
+    paint.color = backColor
+    save()
+    translate(0f, -barH / 2 + (fontSize / 2 + barH / 2) * (1f - sf2))
+    drawText(text, -tw / 2, -fontSize / 4, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawTBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawTextBar(scale, w, h, paint)
+}
+
