559da508c5602e860e41306dadf05e6b761330ea
Anwesh43 <anweshthecool0@gmail.com>
2021-01-02 16:40:49 +0530

    adding TBNode class

--- textbarview/src/main/java/com/example/textbarview/TextBarView.kt
+++ textbarview/src/main/java/com/example/textbarview/TextBarView.kt
@@ -120,4 +120,45 @@ class TextBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TBNode(var i : Int, val state : State = State()) {
+
+        private var next : TBNode? = null
+        private var prev : TBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TBNode {
+            var curr : TBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
